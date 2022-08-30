844273ccd7bfd7dc738cc0dd6f9df9327f340ede
Brian <brianzheng927@gmail.com>
2022-08-30 11:30:58 -0700

    143

--- Neetcode75.py
+++ Neetcode75.py
@@ -1,3 +1,6 @@
+#Binary Search
+#153. (Medium) Find Minimum in Rotated Sorted Array
+#Time: O(logN), Space: O(1)
 def findMin(self, nums: List[int]) -> int:
         left, right = 0, len(nums)-1
         while left <= right:
@@ -9,3 +12,41 @@ def findMin(self, nums: List[int]) -> int:
             else:
                 right = mid - 1         #in the right half
         return nums[0]
+def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        return nums[left]
+
+#Linked List
+#143. Reorder List
+def reorderList(self, head: Optional[ListNode]) -> None:
+        #get midpoint pointer
+        slow, fast = head, head.next
+        while fast:
+            slow = slow.next
+            fast = fast.next
+            if fast:
+                fast = fast.next
+        
+        #reverse second half
+        prev, curr = None, slow
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
+        #construct new LL
+        ptr = head
+        while prev.next:
+            p1, p2 = ptr.next, prev.next
+            ptr.next = prev
+            prev.next = p1
+            ptr, prev = p1, p2
+
+        
