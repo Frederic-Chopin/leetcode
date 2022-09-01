def87ad1812e21b000b76b3d7ccfbd6cecab02ac
Brian <brianzheng927@gmail.com>
2022-09-01 08:44:11 -0700

    235

--- tree&graph.py
+++ tree&graph.py
@@ -76,3 +76,44 @@ def isSubtree(self, s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
     if not s: return
     if s.val == t.val and dfs(s, t): return True
     return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+#(Merkle hashing)
+#O(|s| + |t|) 
+def isSubtree(self, s, t):
+    from hashlib import sha256
+    def hash_(x):
+        S = sha256()
+        S.update(x)
+        return S.hexdigest()
+        
+    def merkle(node):
+        if not node:
+            return '#'
+        m_left = merkle(node.left)
+        m_right = merkle(node.right)
+        node.merkle = hash_(m_left + str(node.val) + m_right)
+        return node.merkle
+        
+    merkle(s)
+    merkle(t)
+    def dfs(node):
+        if not node:
+            return False
+        return (node.merkle == t.merkle or 
+                dfs(node.left) or dfs(node.right))
+                    
+    return dfs(s)
+
+
+#235. Lowest Common Ancestor of a Binary Search Tree
+#Time: O(N); Space: O(N), worset case skewed
+def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        parent_val = root.val
+        p_val = p.val
+        q_val = q.val
+
+        if p_val > parent_val and q_val > parent_val:    
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif p_val < parent_val and q_val < parent_val:    
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
