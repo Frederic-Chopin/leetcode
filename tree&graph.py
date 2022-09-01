9a62142fe2b4de9f0057589781093679eff36be5
Brian <brianzheng927@gmail.com>
2022-08-31 20:14:45 -0700

    104

--- tree&graph.py
+++ tree&graph.py
@@ -1,10 +1,17 @@
 #Tree
+#226. Invert Binary Tree
 #Recursion
 #Time: O(N), N:#of nodes; Space: O(h), h:height
 def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return root
-        
         root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
-
         return root
+
+#104. Maximum Depth of Binary Tree
+#Recursion
+#Time: O(N); Space: O(N), worst case unbalanced
+def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
