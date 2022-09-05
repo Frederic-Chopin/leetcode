77d1300e7185b9d6e8f56e233bc7402981077b1e
Brian <brianzheng927@gmail.com>
2022-09-04 17:34:38 -0700

    105

--- tree&graph.py
+++ tree&graph.py
@@ -260,3 +260,18 @@ def kthSmallest(self, root, k):
         return root
 
 
+#105. Construct Binary Tree from Preorder and Inorder Traversal
+#Recursion: 
+# preorder[0] is root, 
+# left of inorder is root.left, right of inorder is root.right
+#Time: O(N); Space: O(N)
+def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+    if not preorder or not inorder:
+        return None
+    root = TreeNode(preorder[0])
+    mid = inorder.index(preorder[0])
+    root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+    root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+    return root
+
+
